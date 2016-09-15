36e54f9bbe26f9af0765d959e8b1d4ebf7b26fa1
Jake Higginbotham <jake@Jakes-MacBook.local>
2016-09-15 18:56:46 -0400

    install apache

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-
-apt-get update
-apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
+sleep 20
+sudo apt-get update
+sudo apt-get install -y apache2
+if ! [ -L /var/www/html ]; then
+  rm -rf /var/www/html
+  ln -fs /media/psf/vagrant/html /var/www/html
 fi
